6e7da47c45092d926677f0021bb179a48819dd5d
MindaRyn <natcha.p@ku.th>
2017-03-30 01:51:10 +0700

    Add Box Item and Hide Box

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour {
+
+	public float maxHight,minHight;
+	public Rigidbody rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y >= 20 ||(transform.position.y <= 5)) {
+			speed = speed * (-1);
+		}
+
+		rb.MovePosition(transform.position + new Vector3(0.0f,speed,0.0f));
+	}
+}
