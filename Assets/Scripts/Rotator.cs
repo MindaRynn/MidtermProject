a23fc27f4bfab212eb62009cffcf7a1ddf2eb5d4
MindaRyn <natcha.p@ku.th>
2017-04-05 15:33:45 +0700

    stage 2 finish

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour {
+	public int x,y,z;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +12,6 @@ public class Rotator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (new Vector3 (0, 35, 0) * Time.deltaTime);
+		transform.Rotate (new Vector3 (x, y, z) * Time.deltaTime);
 	}
 }
