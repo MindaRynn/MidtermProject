9401bb8e5981dfc7b063b69efc18ff85569885e9
Varis <win3455@hotmail.com>
2017-04-06 04:00:20 +0700

    Fix the bug that bots can damage each other

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -94,14 +94,17 @@ public class PlayerController : MonoBehaviour {
 		if (bulletCount > 0 && bulletDelayCount <= 0) {
 			bulletCount--;
 			bulletDelayCount = bulletDelay;
+			BulletController bullet;
 			if (IsFacingRight ()) {
-				BulletController bullet = Instantiate (bulletObj, new Vector3 (transform.position.x + 1f, transform.position.y + 1, transform.position.z), Quaternion.identity).gameObject.GetComponentInParent<BulletController> ();
+				bullet = Instantiate (bulletObj, new Vector3 (transform.position.x + 1f, transform.position.y + 1, transform.position.z), Quaternion.identity).gameObject.GetComponentInParent<BulletController> ();
 				bullet.direction = "Right";
+
 			}
 			else {
-				BulletController bullet = Instantiate (bulletObj, new Vector3 (transform.position.x - 1f, transform.position.y + 1, transform.position.z), Quaternion.identity).gameObject.GetComponentInParent<BulletController> ();
+				bullet = Instantiate (bulletObj, new Vector3 (transform.position.x - 1f, transform.position.y + 1, transform.position.z), Quaternion.identity).gameObject.GetComponentInParent<BulletController> ();
 				bullet.direction = "Left";
 			}
+			bullet.shooter = "Player";
 			Debug.Log ("Bullet Left: " + bulletCount);
 		}
 		else {
