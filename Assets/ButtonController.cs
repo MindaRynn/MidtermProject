9129c43cbbc887fbcc258ad3f91d2550ee3f6ec0
MindaRyn <natcha.p@ku.th>
2017-04-08 04:53:46 +0700

    Main menu finish

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour {
+	public Button startBut;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void hover(){
+		startBut.gameObject.GetComponentInChildren<Image>().color = new Color(0,0,0, 0.56F);
+	}
+}
---
+++ Assets/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+	public GameObject faderObj;
+	private Fader fader;
+	// Use this for initialization
+	void Start () {
+		fader = faderObj.GetComponent<Fader> ();
+		fader.gameObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame(){
+		PlayerPrefs.SetString ("PlayerPos", 0 + "," + 0.7);		
+		PlayerPrefs.SetInt ("data1", 0);
+		PlayerPrefs.SetInt ("data2", 0);
+		PlayerPrefs.SetInt ("data3", 0);
+		PlayerPrefs.SetInt ("data4", 0);
+		PlayerPrefs.SetInt ("data5", 0);
+		PlayerPrefs.SetInt ("moveCam1", 0);
+		fader.gameObject.SetActive (true);
+		fader.FadeOutAndLoad ("MainStage1");
+	}
+}
