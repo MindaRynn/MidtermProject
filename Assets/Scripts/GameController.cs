7bc2b27e4493ca5c31a3f91b66ba5a36b8181589
Varis <win3455@hotmail.com>
2017-04-09 22:45:38 +0700

    Adjust invisible wall

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -26,18 +26,18 @@ public class GameController : MonoBehaviour {
 	}
 
 	void Awake () {
-		PlayerPrefs.SetString ("PlayerPos", 0 + "," + 0.7);		
-		PlayerPrefs.SetInt ("data1", 0);
-		PlayerPrefs.SetInt ("data2", 0);
-		PlayerPrefs.SetInt ("data3", 0);
-		PlayerPrefs.SetInt ("data4", 0);
-		PlayerPrefs.SetInt ("data5", 0);
-		PlayerPrefs.SetInt ("data6", 0);
-		PlayerPrefs.SetInt ("data7", 0);
-		PlayerPrefs.SetInt ("moveCam1", 0);
-		PlayerPrefs.SetInt ("moveCam2", 0);
-		PlayerPrefs.SetInt ("moveCam3", 0);
-		PlayerPrefs.SetInt ("bossChanging", 0);
+//		PlayerPrefs.SetString ("PlayerPos", 0 + "," + 0.7);		
+//		PlayerPrefs.SetInt ("data1", 0);
+//		PlayerPrefs.SetInt ("data2", 0);
+//		PlayerPrefs.SetInt ("data3", 0);
+//		PlayerPrefs.SetInt ("data4", 0);
+//		PlayerPrefs.SetInt ("data5", 0);
+//		PlayerPrefs.SetInt ("data6", 0);
+//		PlayerPrefs.SetInt ("data7", 0);
+//		PlayerPrefs.SetInt ("moveCam1", 0);
+//		PlayerPrefs.SetInt ("moveCam2", 0);
+//		PlayerPrefs.SetInt ("moveCam3", 0);
+//		PlayerPrefs.SetInt ("bossChanging", 0);
 		sound = alertSound.GetComponent<AudioSource> ();
 		dialogueManager = FindObjectOfType<DialogueManager> ();
 		if (textFile != null) {
